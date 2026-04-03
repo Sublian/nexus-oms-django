14f4602ab704cb0dd77dfc173ed350c2320ae535
Luis Gonzalez <subliangel@gmail.com>
2026-04-02 22:33:50 -0500

    1. model(org): add ruc, address and color fields to Organization model

--- src/domain/models/base.py
+++ src/domain/models/base.py
@@ -16,6 +16,8 @@ class Organization(models.Model):
     logo_image = models.ImageField(upload_to='organization_logos/', blank=True, null=True) # Imagen del logo de la empresa
     dashboard_batch_size = models.PositiveIntegerField(default=10, help_text="Registros por página en el dashboard")
     currency_symbol = models.CharField(max_length=5, default="$")
+    ruc = models.CharField(max_length=11, default="00000000000") # RUC para Perú, ajustable para otros países
+    address = models.TextField(default="Dirección por defecto")
 
     def __str__(self):
         return self.name
